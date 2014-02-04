dc374b7b18efee311aff656122eca3e9705001b2
Thunder Stumpges <tstumpges@verticalsearchworks.com>
2014-02-04 14:44:31 -0800

    Fix namespace using clause and "friend assembly" (InternalsVisibleTo) for Cassandra.MSTest so that solution compiles.

--- Cassandra/MemoryTributary.cs
+++ Cassandra/MemoryTributary.cs
@@ -6,7 +6,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+﻿using System.Runtime.InteropServices;
 
 namespace System.IO
 {
